2653655486fb33aeec5e7178b33732c44c37b5ba
Maksym <m.fomichov@gmail.com>
2017-06-28 01:47:41 +0300

    добавил запрос данных

--- src/work/Main.java
+++ src/work/Main.java
@@ -1,5 +1,7 @@
 package work;
 
+import java.util.Scanner;
+
 /**
  * Created by mfomi on 28.06.2017.
  */
@@ -7,5 +9,19 @@ public class Main {
     public static void main (String[] args){
         String name, city, hobby;
         int age;
+
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("Введите Ваше имя");
+        name = scanner.nextLine();
+        System.out.println("Введите Ваш город проживания");
+        city = scanner.nextLine();
+        System.out.println("Введите Ваш возраст");
+        age = scanner.nextInt();
+        System.out.println("Введите Ваше хобби");
+        hobby = scanner.nextLine();
+
+        
+
+
     }
 }
